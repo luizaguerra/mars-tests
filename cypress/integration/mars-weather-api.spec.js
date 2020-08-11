8f5dfd26be744daa7d8b193ad6255fbcaef0b8c1
Luiza Guerra <luiza.guerra@gmail.com>
2020-08-10 21:14:41 -0300

    chore: include baseUrl config and change reference on spec

--- cypress/integration/mars-weather-api.spec.js
+++ cypress/integration/mars-weather-api.spec.js
@@ -2,7 +2,7 @@
 
 describe('tests Mars weater api', () =>{
     it('should return data of the first measure', () => {
-        cy.request('https://api.mars.spacexcompanion.app/v1/weather/first').should((response) =>{
+        cy.request('/first').should((response) =>{
             expect(response.status).to.equal(200)
             expect(response.body.season).to.equal('spring')
             expect(response.body.wind.directions).to.have.length(14)
@@ -10,7 +10,7 @@ describe('tests Mars weater api', () =>{
     })
 
     it('should return data of the latest measure', () => {
-        cy.request('https://api.mars.spacexcompanion.app/v1/weather/latest').should((response) =>{
+        cy.request('/latest').should((response) =>{
             expect(response.status).to.equal(200)
             expect(response.body.season).to.equal('summer')
             cy.fixture('mars-weather-first-measure').should('deep.equal', (response.body))
