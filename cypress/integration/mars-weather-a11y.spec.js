1ccb02c2950fbf73feef6d4ae24de03a7e1d3ada
Luiza Guerra <luiza.guerra@gmail.com>
2020-08-10 23:04:58 -0300

    test: refactor acessibility verification to use a custom command

--- cypress/integration/mars-weather-a11y.spec.js
+++ cypress/integration/mars-weather-a11y.spec.js
@@ -1,31 +1,9 @@
 describe('A11y passes', () => {
-    
-    function terminalLog(violations) {
-        cy.task(
-          'log',
-          `${violations.length} accessibility violation${
-            violations.length === 1 ? '' : 's'
-          } ${violations.length === 1 ? 'was' : 'were'} detected`
-        )
-        const violationData = violations.map(
-          ({ id, impact, description, nodes }) => ({
-            id,
-            impact,
-            description,
-            nodes: nodes.length
-          })
-        )
-      
-        cy.task('table', violationData)
-      }
-
     beforeEach(() => {
       cy.visit('https://api.mars.spacexcompanion.app/')
     })
   
     it('accessibility check', () => {
-      cy.contains('Mars Weather API 1.0.1')
-      cy.injectAxe()
-      cy.checkA11y(null, null, terminalLog, true)
+      cy.checkAcessibility()
     })
   })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -10,16 +10,26 @@
 //
 //
 // -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+Cypress.Commands.add("checkAcessibility", () => {
+    function terminalLog(violations) {
+        cy.task(
+          'log',
+          `${violations.length} accessibility violation${
+            violations.length === 1 ? '' : 's'
+          } ${violations.length === 1 ? 'was' : 'were'} detected`
+        )
+        const violationData = violations.map(
+          ({ id, impact, description, nodes }) => ({
+            id,
+            impact,
+            description,
+            nodes: nodes.length
+          })
+        )
+        cy.task('table', violationData)
+      }
+      
+      cy.contains('Mars Weather API 1.0.1')
+      cy.injectAxe()
+      cy.checkA11y(null, null, terminalLog, true)
+})
